4a081c50999cbd79e036c8f7144fa77e42f3d956
NellyNel520 <channelharris520@gmail.com>
2023-04-14 23:46:20 -0400

    created product deatils page with mock/ temp data

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,20 @@ import './styles/App.css'
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Footer from './components/Footer';
+import Announcement from './components/Announcement';
+import Products from './pages/Products';
+import ProductDetails from './pages/ProductDetails';
 
 function App() {
   return (
      <div>
       <Navbar />
+      <Announcement />
       <main>
       <Routes>
-        <Route path="/" element={<Home />} />
+        {/* <Route path="/" element={<Home />} /> */}
+        {/* <Route path="/" element={<Products />} /> */}
+         <Route path="/" element={<ProductDetails />} />
       </Routes>
       </main>
       <Footer />
---
+++ client/src/pages/Products.js
@@ -0,0 +1,78 @@
+import React from 'react'
+import styled from 'styled-components'
+import FeaturedProducts from '../components/FeaturedProducts';
+import Newsletter from '../components/Newsletter';
+const Container = styled.div`
+
+`
+const Title = styled.h1`
+  margin: 20px;
+`;
+
+const FilterContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Filter = styled.div`
+  margin: 20px 35px 20px 35px;
+`;
+
+const FilterText = styled.span`
+  font-size: 20px;
+  font-weight: 600;
+  margin-right: 20px;
+  color: white;
+  ${'' /* ${mobile({ marginRight: "0px" })} */}
+`;
+
+const Select = styled.select`
+  padding: 3px;
+  margin-right: 20px;
+  border-radius: 5px;
+  ${'' /* ${mobile({ margin: "10px 0px" })} */}
+`;
+const Option = styled.option``;
+
+
+const Products = () => {
+  return (
+    <Container className='font-play'>
+    <Title className='text-6xl text-center pt-5 text-blue-400 font-play'>Category</Title>
+    <FilterContainer>
+      <Filter >
+        <FilterText>Filter Products:</FilterText>
+        <Select name="color" >
+          <Option disabled>Color</Option>
+          <Option>white</Option>
+          <Option>black</Option>
+          <Option>red</Option>
+          <Option>blue</Option>
+          <Option>yellow</Option>
+          <Option>green</Option>
+        </Select>
+        <Select name="size" >
+          <Option disabled >Size</Option>
+          <Option>XS</Option>
+          <Option>S</Option>
+          <Option>M</Option>
+          <Option>L</Option>
+          <Option>XL</Option>
+        </Select>
+      </Filter>
+      <Filter>
+        <FilterText>Sort Products:</FilterText>
+        <Select>
+          <Option value="newest">Newest</Option>
+          <Option value="asc">Price (asc)</Option>
+          <Option value="desc">Price (desc)</Option>
+        </Select>
+      </Filter>
+    </FilterContainer>
+    <FeaturedProducts />
+    <Newsletter />
+  </Container>
+  )
+}
+
+export default Products
