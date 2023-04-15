ff36edaacc665e6e971c114407b97feeea732927
NellyNel520 <channelharris520@gmail.com>
2023-04-15 18:37:24 -0400

    created product model

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose')
 const UserSchema = require('./user')
+const ProductSchema = require('./product')
 
 const User = mongoose.model('User', UserSchema)
+const Product = mongoose.model('Product', ProductSchema)
 
 module.exports = {
   User,
+  Product,
 }
