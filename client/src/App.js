07b3bba73d338140b6850c8520ad11803eb956d3
NellyNel520 <channelharris520@gmail.com>
2023-04-14 20:53:16 -0400

    addded browser router and created navbar and home page files

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,21 @@
-
+import { Route, Routes } from 'react-router-dom'
 import './App.css';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-     app
-    </div>
+     <div>
+      <Navbar />
+      <main>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+      </main>
+      {/* footer */}
+
+     </div>
+    
   );
 }
 
