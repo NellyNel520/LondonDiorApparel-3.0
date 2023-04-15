8d452d432275607854f990a033ac65163d088337
NellyNel520 <channelharris520@gmail.com>
2023-04-15 18:44:23 -0400

    created category model

--- backend/models/index.js
+++ backend/models/index.js
@@ -2,13 +2,17 @@ const mongoose = require('mongoose')
 const UserSchema = require('./user')
 const ProductSchema = require('./product')
 const OrderSchema = require('./order')
+const CategorySchema = require('./category')
 
 const User = mongoose.model('User', UserSchema)
 const Product = mongoose.model('Product', ProductSchema)
 const Order = mongoose.model('Order', OrderSchema)
+const Category = mongoose.model('Category', CategorySchema)
 
 module.exports = {
   User,
   Product,
   Order,
+  Category,
+  
 }
