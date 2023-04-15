d8ebdd9d023d0253831c82d60d3714ed452b798d
NellyNel520 <channelharris520@gmail.com>
2023-04-15 18:31:47 -0400

    created user model

---
+++ backend/models/user.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema
+
+const User = new Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = User
