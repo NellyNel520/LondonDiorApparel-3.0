4358de8b53e85df15844bf058f75bcc192b41678
NellyNel520 <channelharris520@gmail.com>
2023-04-15 18:46:11 -0400

    created cart model

--- backend/models/index.js
+++ backend/models/index.js
@@ -3,16 +3,19 @@ const UserSchema = require('./user')
 const ProductSchema = require('./product')
 const OrderSchema = require('./order')
 const CategorySchema = require('./category')
+const CartSchema = require('./cart')
 
 const User = mongoose.model('User', UserSchema)
 const Product = mongoose.model('Product', ProductSchema)
 const Order = mongoose.model('Order', OrderSchema)
 const Category = mongoose.model('Category', CategorySchema)
+const Cart = mongoose.model('Cart', CartSchema)
 
 module.exports = {
   User,
   Product,
   Order,
   Category,
-  
+  Cart
+
 }
