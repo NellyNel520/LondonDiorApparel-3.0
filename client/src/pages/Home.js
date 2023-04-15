2b2e7c5ab61d8960a7841879f93649d94ea338ec
NellyNel520 <channelharris520@gmail.com>
2023-04-14 22:52:07 -0400

    created featured products component / section

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Slider from '../components/Slider'
 import Categories from '../components/Categories'
+import FeaturedProducts from '../components/FeaturedProducts'
 // import LogoHeader from '../components/LogoHeader'
 
 const Home = () => {
@@ -9,6 +10,7 @@ const Home = () => {
       {/* <LogoHeader /> */}
 			<Slider />
 			<Categories />
+			<FeaturedProducts />
 		</div>
 	)
 }
