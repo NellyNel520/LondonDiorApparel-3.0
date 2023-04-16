4f51e3c03191c9a2f10fc039e82e1158915c3382
NellyNel520 <channelharris520@gmail.com>
2023-04-15 23:45:21 -0400

    admin only routes for get user by id and get all users successful via insomnia testing

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -24,6 +24,7 @@ const updateUser = async (req, res) => {
   }
 };
 
+// DELETE
 const deleteUser = async (req, res) => {
   try {
     await User.findByIdAndDelete(req.params.id);
@@ -33,7 +34,35 @@ const deleteUser = async (req, res) => {
   }
 }
 
+// GET USER BY ID
+const getUserById = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, ...others } = user._doc;
+    res.status(200).json(others);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+}
+
+// GET ALL USERS
+const getAllUsers = async (req, res) => {
+  const query = req.query.new;
+  try {
+    const users = query
+      ? await User.find().sort({ _id: -1 }).limit(5)
+      : await User.find();
+    res.status(200).json(users);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+}
+
+
+
 module.exports = {
   updateUser,
   deleteUser,
+  getUserById,
+  getAllUsers,
 }
--- backend/routes/UserRouter.js
+++ backend/routes/UserRouter.js
@@ -1,14 +1,14 @@
 const router = require('express').Router()
 const controller = require('../controllers/UserController')
 const {
-  verifyToken,
-  verifyTokenAndAuthorization,
-  verifyTokenAndAdmin,
-} = require("../controllers/VerifyToken")
+	verifyToken,
+	verifyTokenAndAuthorization,
+	verifyTokenAndAdmin,
+} = require('../controllers/VerifyToken')
 
- 
-router.put("/:id", verifyTokenAndAuthorization,  controller.updateUser)
-router.delete("/:id", verifyTokenAndAuthorization,  controller.deleteUser)
-
+router.put('/:id', verifyTokenAndAuthorization, controller.updateUser)
+router.delete('/:id', verifyTokenAndAuthorization, controller.deleteUser)
+router.get('/find/:id', verifyTokenAndAdmin, controller.getUserById)
+router.get('/all', verifyTokenAndAdmin, controller.getAllUsers)
 
 module.exports = router
