13130649f6a20a4c69a61d88982eb83af9ebdc61
NellyNel520 <channelharris520@gmail.com>
2023-04-15 22:26:08 -0400

    created login function and route, successful, tested with insomnia

--- backend/controllers/AuthController.js
+++ backend/controllers/AuthController.js
@@ -22,7 +22,39 @@ const Register = async (req, res) => {
 
 }
 
+const Login = async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email, });
+    !user && res.status(401).json("Wrong credentials!");
+
+    const hashedPassword = CryptoJS.AES.decrypt(
+      user.password,
+      process.env.APP_SEC
+    );
+    const OriginalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+
+    OriginalPassword !== req.body.password &&
+      res.status(401).json("Wrong credentials!");
+
+    const accessToken = jwt.sign(
+      {
+        id: user._id,
+        isAdmin: user.isAdmin,
+      },
+      process.env.JWT_SEC,
+      {expiresIn:"3d"}
+    );
+
+    const { password, ...others } = user._doc;
+
+    res.status(200).json({...others, accessToken});
+  } catch (err) {
+    res.status(500).json(err);
+  }
+}
+
 module.exports = {
   Register,
+  Login
 
 }
--- backend/routes/AuthRouter.js
+++ backend/routes/AuthRouter.js
@@ -2,6 +2,7 @@ const router = require('express').Router()
 const controller = require('../controllers/AuthController')
 
 router.post('/register', controller.Register)
+router.post('/login', controller.Login)
 
 module.exports = router
 
