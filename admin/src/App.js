0e7528a5691ad864d6c129636002454bbffc8f23
NellyNel520 <channelharris520@gmail.com>
2023-04-21 22:11:16 -0400

    creating structure for admin portal

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,76 +1,9 @@
-import { Route, Routes } from 'react-router-dom'
-import './styles/App.css'
-import Navbar from './components/Navbar';
-import Home from './pages/Home';
-import Footer from './components/Footer';
-import Announcement from './components/Announcement';
-import Products from './pages/Products';
-import ProductDetails from './pages/ProductDetails';
-import Newsletter from './components/Newsletter';
-import Register from './pages/Register';
-import Login from './pages/Login';
-import Cart from './pages/Cart';
-import About from './pages/About';
-import { useSelector, useDispatch } from "react-redux";
-import { logout, clearCart } from './redux/userRedux';
-// import { clearCart } from "./redux/cartRedux";
-import { useNavigate } from 'react-router-dom'
-import Success from './pages/Success';
+import React from 'react'
 
-
-
-
-function App() {
-  const user = useSelector((state) => state.user.currentUser);
-
-  const dispatch = useDispatch()
-  let navigate = useNavigate()
-
-  const handleLogOut = (e) => {
-		//Reset all auth related state and clear localStorage
-    e.preventDefault();
-    dispatch(logout())
-    navigate("/")
-	}
-
-  // const clearCart = (e) => {
-	// 	//Reset all auth related state and clear localStorage
-  //   e.preventDefault();
-  //   dispatch(clearCart())
-  //   navigate("/cart")
-	// }
-
-
-	// // Clear Cart
-	// const clearCart = (e) => {
-  //     e.preventDefault();
-	//   dispatch(clearCart());
-	// };
-
-  
+const App = () => {
   return (
-     <div>
-      <Navbar user={user} handleLogOut={handleLogOut}/>
-      <Announcement user={user} />
-      <main>
-      <Routes>
-        <Route path="/" element={<Home />} />
-         <Route path="/register" element={<Register />} />
-         <Route path="/login" element={<Login />} />
-        <Route path="/products/:category" element={<Products />} />
-        <Route path="/products" element={<Products />} />
-         <Route path="/product/:id" element={<ProductDetails />} />
-         <Route path="/cart" element={<Cart />} />
-         <Route path="/about" element={<About />} />
-         <Route path="/success" element={<Success />} />
-      </Routes>
-      </main>
-      <Newsletter />
-      <Footer />
-
-     </div>
-    
-  );
+    <div className='text-white text-2xl'>App</div>
+  )
 }
 
-export default App;
+export default App
--- admin/src/index.js
+++ admin/src/index.js
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
-import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { store, persistor } from './redux/store'
 import { PersistGate } from 'redux-persist/integration/react'
@@ -11,9 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
 	<Provider store={store}>
 		<PersistGate loading={null} persistor={persistor}>
-			<BrowserRouter>
 				<App />
-			</BrowserRouter>
 		</PersistGate>
 	</Provider>
 )
