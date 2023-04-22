3d163b3479eaeed1847d7e516ad1f90bbe97d179
NellyNel520 <channelharris520@gmail.com>
2023-04-22 17:58:06 -0400

    admin app base structure setup

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,10 +1,33 @@
 import { Route, Routes } from 'react-router-dom'
-import './styles/App.css'
 import React from 'react'
+import Sidebar from "./components/sidebar/Sidebar";
+import Topbar from "./components/topbar/Topbar";
+import "./styles/App.css";
+import Home from "./pages/home/Home";
+import UserList from "./pages/userList/UserList";
+import User from "./pages/user/User";
+import NewUser from "./pages/newUser/NewUser";
+import ProductList from "./pages/productList/ProductList";
+import Product from "./pages/product/Product";
+import NewProduct from "./pages/newProduct/NewProduct";
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Topbar />
+      <main className='container'>
+        <Sidebar />
+        <Routes>
+          <Route path="/" element={ <Home />} />
+          <Route path="/users" element={ <UserList />} />
+          <Route path="/user/:userId" element={ <User />} />
+          <Route path="/newUser" element={ <NewUser />} />
+          <Route path="/products" element={ <Product />} />
+          <Route path="/newproduct" element={ <NewProduct />} />
+        </Routes>
+       
+      </main>
+    </div>
   )
 }
 
