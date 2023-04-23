375a82b81684798745622385ac527625898a4cf1
NellyNel520 <channelharris520@gmail.com>
2023-04-23 19:59:33 -0400

    added top and side bar to productlist

--- admin/src/App.js
+++ admin/src/App.js
@@ -25,7 +25,8 @@ const App = () => {
 				<Route path="/users" element={<UserList />} />
 				<Route path="/user/:userId" element={<User />} />
 				<Route path="/newUser" element={<NewUser />} />
-				<Route path="/products" element={<Product />} />
+				<Route path="/product/:productId" element={<Product />} />
+        <Route path="/products" element={<ProductList />} />
 				<Route path="/newproduct" element={<NewProduct />} />
 			</Routes>
 		</div>
